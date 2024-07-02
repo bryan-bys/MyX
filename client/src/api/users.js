880c35c2845898730eef4f3e80fc3619da5c6e33
BryanMartinezMesa <bryanmartinezmym@gmail.com>
2024-07-02 19:57:50 +0200

    users.js modified

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const URL = import.meta.env.VITE_API_URL;
+
 export const userRegister = (data) => {
-  return axios.post("http://127.0.0.1:8000/users/register/", data);
+  return axios.post(`${URL}/users/register/`, data);
 };
 
 export const userLogin = (data) => {
-  return axios.post("http://127.0.0.1:8000/users/login/", data);
+  return axios.post(`${URL}/users/login/`, data);
 };
 
 export const post = (data, token) => {
-  return axios.post("http://127.0.0.1:8000/api/post/", {}, {});
+  return axios.post(`${URL}/api/post/`, {}, {});
 };
