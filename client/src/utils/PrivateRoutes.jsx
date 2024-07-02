e58f7b7f243e1bb7b6f98c049ff48fffde2eafb1
BryanMartinezMesa <bryanmartinezmym@gmail.com>
2024-07-02 22:32:46 +0200

    test privateroutes fix

--- client/src/utils/PrivateRoutes.jsx
+++ client/src/utils/PrivateRoutes.jsx
@@ -1,8 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
+import Login from "../pages/Login";
 
 export const PrivateRoutes = ({ children, user }) => {
   if (!localStorage.getItem("token")) {
-    return <Navigate to="/login" />;
+    return <Navigate to={<Login />} />;
   } else {
     return <Outlet />;
   }
