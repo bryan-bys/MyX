4ac08d9bb123453d3c4132c53d0c734959a83285
BryanMartinezMesa <bryanmartinezmym@gmail.com>
2024-07-30 21:40:52 +0200

    update basedata

--- core/settings.py
+++ core/settings.py
@@ -94,19 +94,18 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    #  'default': dj_database_url.config(
-    #     # Replace this value with your local database's connection string.
-    #      default='postgresql://mycoffeeshop_user:zFbNXjQn0gfThXufHWHpmymZqAjLR4nl@dpg-cpu2oqt2ng1s73ea2c90-a.oregon-postgres.render.com/myx',
-    #     conn_max_age=600
-    # )
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myx',
-        'USER': 'myx_user',
-        'PASSWORD': 'BauCBRk7NW448BggRatHOLyJU4lLDn43',
-        'HOST': 'dpg-cq24ls5umphs73e90a80-a.oregon-postgres.render.com',
-        'PORT': '5432',
-    }
+     'default': dj_database_url.config(
+         default='postgresql://myx_kj0l_user:23qFrwK2sxxt0c1Oz0pf0BlTHcDiOgLn@dpg-cqkjl4ks1f4s73d65d2g-a.oregon-postgres.render.com/myx_kj0l',
+        conn_max_age=600
+    )
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'myx_kj0l',
+    #     'USER': 'myx_kj0l_user',
+    #     'PASSWORD': '23qFrwK2sxxt0c1Oz0pf0BlTHcDiOgLn',
+    #     'HOST': 'dpg-cqkjl4ks1f4s73d65d2g-a.oregon-postgres.render.com',
+    #     'PORT': '5432',
+    # }
   
 }
 
