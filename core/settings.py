344292701ff5535498a2e1bd1fad13bf5b6fa594
BryanMartinezMesa <bryanmartinezmym@gmail.com>
2024-09-05 19:22:19 +0200

    update likes render

--- core/settings.py
+++ core/settings.py
@@ -90,14 +90,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #  'default': dj_database_url.config(
-    #      default='postgresql://myx_kj0l_user:23qFrwK2sxxt0c1Oz0pf0BlTHcDiOgLn@dpg-cqkjl4ks1f4s73d65d2g-a.oregon-postgres.render.com/myx_kj0l',
-    #     conn_max_age=600
-    # )
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+     'default': dj_database_url.config(
+         default='postgresql://myx_kj0l_user:23qFrwK2sxxt0c1Oz0pf0BlTHcDiOgLn@dpg-cqkjl4ks1f4s73d65d2g-a.oregon-postgres.render.com/myx_kj0l',
+        conn_max_age=600
+    )
     #     'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'myx_c8vp',
